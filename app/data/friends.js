0a3952e4aa1fbab9a8a5465dba692e9f51900533
clo2go <clo2go@gmail.com>
2017-01-31 06:30:29 -0600

    new friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,12 +3,42 @@
 module.exports = [
 	{
 		name: "Skeeter",
-		photo: "https://dominoartsphotographyandcinematography.files.wordpress.com/2012/08/headshot2.jpg",
+		photo: "http://www.vladtv.com/images/size_fs/video_image-100973.jpg",
 		scores: [4, 2, 3, 5, 5, 2, 5, 2, 1, 5]
 	},
 	{
-		name: "Big Mike",
+		name: "Fronky Lonk",
 		photo: "http://images.rcp.realclearpolitics.com/253229_5_.jpg",
+		scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+	}
+	{
+		name: "Streep Dog",
+		photo: "https://heavyeditorial.files.wordpress.com/2013/04/413.jpg?quality=65&strip=all",
+		scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+	}
+	{
+		name: "Big Perm",
+		photo: "https://d1w5usc88actyi.cloudfront.net/wp-content/uploads/2013/06/glamour-shots-29.jpg",
+		scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+	}
+	{
+		name: "El Arch",
+		photo: "http://cdn77.eatliver.com/wp-content/uploads/2016/09/cheap1.jpg",
+		scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+	}
+	{
+		name: "Carbi",
+		photo: "http://www.rantchic.com/wp-content/uploads/2015/10/bad-glamour-shots-5.jpg",
+		scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+	}
+	{
+		name: "Snatch",
+		photo: "https://dominoartsphotographyandcinematography.files.wordpress.com/2012/08/headshot2.jpg",
+		scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+	}
+	{
+		name: "PJ",
+		photo: "https://s-media-cache-ak0.pinimg.com/originals/de/19/6d/de196d2771211d076c24324d678559b6.jpg",
 		scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
 	}
 ];
